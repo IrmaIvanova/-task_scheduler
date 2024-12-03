6988b4406ff56fcba569b488a69dd3f92b450b9a
Irma <ii@pryaniky.ru>
2024-12-03 17:51:02 +0300

    add typescript

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+interface AppProps {
+  message: string;
+}
+
+const App: React.FC<AppProps> = ({ message }) => {
+  return <h1>{message}</h1>;
+};
+
+ReactDOM.render(
+  <App message='Hello, world!' />,
+  document.getElementById('root')
+);
