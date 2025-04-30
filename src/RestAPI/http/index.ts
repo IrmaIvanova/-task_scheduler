322d4916b7ba8090943965bf462e2c46a7050d77
Irma <ii@pryaniky.ru>
2025-04-30 18:05:29 +0300

    fix: add allow-origin to headers

--- src/RestAPI/http/index.ts
+++ src/RestAPI/http/index.ts
@@ -5,11 +5,16 @@ export const API_URL =`http://localhost:5000/api`
 
 const $api = axios.create({
     withCredentials: true,
-    baseURL: API_URL
+    baseURL: API_URL,
+    headers:{
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    }
 })
 
 $api.interceptors.request.use((config)=>{
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+ 
     return config
 })
 
