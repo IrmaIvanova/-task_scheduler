a91debec49dd63f16a262eb2545bc437e9b9e57d
Irma <ii@pryaniky.ru>
2025-06-20 16:40:27 +0300

    connect to vercel back

--- src/RestAPI/http/index.ts
+++ src/RestAPI/http/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const API_URL =`http://localhost:5000/api`
-// export const API_URL =`https://backend-one-beta-73.vercel.app/api`
+// export const API_URL =`http://localhost:5000/api`
+export const API_URL =`https://backend-one-beta-73.vercel.app/api`
 // export const API_URL =`https://backend-git-dev-irmas-projects-89b5f685.vercel.app/api`
 
 
