ff7497a1c358e3ffd4bd06de682a35ca8334fa05
Irma <ii@pryaniky.ru>
2025-06-20 16:35:35 +0300

    no allow headers

--- src/RestAPI/http/index.ts
+++ src/RestAPI/http/index.ts
@@ -1,16 +1,14 @@
 import axios from 'axios'
 
-export const API_URL =`https://backend-one-beta-73.vercel.app/api`
+export const API_URL =`http://localhost:5000/api`
+// export const API_URL =`https://backend-one-beta-73.vercel.app/api`
 // export const API_URL =`https://backend-git-dev-irmas-projects-89b5f685.vercel.app/api`
 
 
 const $api = axios.create({
     withCredentials: true,
     baseURL: API_URL,
-    headers:{
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
-    }
+   
 })
 
 $api.interceptors.request.use((config)=>{
